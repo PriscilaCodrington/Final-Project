68bc1312b6e583b35ac05dd9318d6038c010ab42
PriscilaCodrington <priscilacodrington@gmail.com>
2023-05-18 20:29:44 +0200

    La API funciona

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination } from 'vue3-carousel'
 import mainHeader from './components/includes/mainHeader.vue'
 import lateralMenu from './components/includes/lateralMenu.vue'
 import principalHeader from './components/Parts/principalHeader.vue'
+
 
 export default{
   name: 'App',
   components:{
     mainHeader,
     lateralMenu,
-    principalHeader
+    principalHeader,
+    Carousel,
+    Slide,
+    Pagination,
+   
   }
 }
 </script>
@@ -17,7 +24,16 @@ export default{
   <mainHeader />
   <lateralMenu />
   <principalHeader />
-  
+  <carousel :items-to-show="1">
+    <!-- <slide v-for="slide in 10" :key="slide">
+      {{ slide }}
+    </slide> -->
+
+    <template #addons>
+      
+      <pagination />
+    </template>
+  </carousel>
 </template>
 
 <style>
